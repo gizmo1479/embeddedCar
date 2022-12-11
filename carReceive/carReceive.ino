25d95d5edc3b1095330b2cf8f289fc2d2b52c279
gizmo1479 <gizmo1479@system76-pc.localdomain>
2022-12-11 16:14:14 -0500

    watchdog is in

--- carReceive/carReceive.ino
+++ carReceive/carReceive.ino
@@ -174,6 +174,9 @@ void setup() {
   }
 
   Serial.println("Connected to server");
+
+  // setup the Watchdog
+  WDT_setup();
 }
 
 
@@ -182,12 +185,14 @@ void loop() {
   // if client disconnects
   while (!client.connected()) {
     // reconnect to server
+    WDT_pet();
     Serial.println("disconnected, attempting to reconnect");
     client.connect(ip, connPort);
     updateFSM(0, NONE, millis());
     delay(1000);
   }
 
+  WDT_pet();
   COLOR color = pollColorSensor();
   char direction = 0;
 
@@ -377,6 +382,43 @@ void powerRightMotors(motorFunction function) {
   digitalWrite(LEFT_MOTOR_PIN2, motor2);
 }
 
+/********** WATCHDOG CODE ***************************/
+void WDT_setup() {
+  // Clear and enable WDT
+  NVIC_DisableIRQ(WDT_IRQn);
+  NVIC_ClearPendingIRQ(WDT_IRQn);
+  NVIC_SetPriority(WDT_IRQn, 0);
+  NVIC_EnableIRQ(WDT_IRQn);
+
+  // Configure WDT GCLK
+  GCLK->GENDIV.reg = GCLK_GENDIV_DIV(4) | GCLK_GENDIV_ID(5);
+  while (GCLK->STATUS.bit.SYNCBUSY);
+
+  // set GCLK->GENCTRL.reg and GCLK->CLKCTRLL.reg
+  GCLK->GENCTRL.reg = GCLK_GENCTRL_SRC_OSCULP32K | GCLK_GENCTRL_ID(0x05) | 
+                      GCLK_GENCTRL_GENEN | GCLK_GENCTRL_DIVSEL;
+  while (GCLK->STATUS.bit.SYNCBUSY);
+
+  GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID(0x03) | GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK5;
+
+  // configure and enable WDT
+  WDT->CONFIG.reg = 0x8; // period of 2048 clock cycles, around 1 second with 1kHz clock
+  WDT->EWCTRL.reg = 0x7; // early warning at 1 sec, period of 1024
+  WDT->CTRL.reg = WDT_CTRL_ENABLE;
+  while(WDT->STATUS.bit.SYNCBUSY);
+  WDT->INTENSET.bit.EW = 1;
+  
+}
+
+
+void WDT_pet() {
+  WDT->CLEAR.reg = 0xa5;
+}
+
+void WDT_Handler() {
+  WDT->INTFLAG.reg = 1;
+  Serial.println("Watchdog about to be reset...");
+}
 
 
 /********** ALL TESTING RELATED CODE BELOW **********/
